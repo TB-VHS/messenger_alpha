473006011bbec30f50cfd8a5373f0c919d570747
TB-VHS <tb.vhs.e@gmail.com>
2023-04-19 09:20:56 +0200

    +login-routes +body-parser

--- app.mjs
+++ app.mjs
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
 import express from 'express'
 import { engine } from 'express-handlebars'
@@ -5,15 +7,29 @@ import { engine } from 'express-handlebars'
 const app = express()
 
 app.use( express.static( './public' ))
+app.use( express.json() )
+app.use( express.urlencoded({ extended: true }) )
 
 app.engine( 'handlebars', engine() )
 app.set( 'view engine', 'handlebars' )
 app.set( 'views', './views' )
+
 
 app.get( '/'
 , ( req, res )=>{
     res.render( 'home', { title: 'Home, mein Pferd' } )
 })
 
+app.get( '/login'
+, ( req, res )=>{
+    res.render( 'login', {})
+})
 
-app.listen( 3000 )
+app.post( '/login'
+, ( req, res )=>{
+    console.log( req.body.username )
+    console.log( req.body.password )
+
+})
+
+app.listen( process.env.HTTP_PORT )
